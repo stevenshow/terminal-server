808c6bdfe890ad84e613cb7f382a9004e540289a
Steven Schoebinger <steven.schoebinger@gmail.com>
2022-07-19 09:38:39 -0600

    fixing cors issue

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+var cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const cardsRouter = require("./routes/cards");
 
 const app = express();
+app.use(cors());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
