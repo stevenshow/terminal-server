8742deaa14e0bbf42674f5346e24ab484cb1db9c
Steven Schoebinger <steven.schoebinger@gmail.com>
2022-07-21 09:46:38 -0600

    attempting to validate webhook payload

--- app.js
+++ app.js
@@ -25,11 +25,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/cards", cardsRouter);
-// TODO figure out how to correctly validate
-// app.use(validatePayload);
-app.use("/deploy", deployRouter);
+app.use("/", require("./routes/routes"));
+// app.use("/", indexRouter);
+// app.use("/cards", cardsRouter);
+// // TODO figure out how to correctly validate
+// // app.use(validatePayload);
+// app.use("/deploy", deployRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
