9d02405b5373559f66714bd96aeca40248d49efa
stevenshow <steven.schoebinger@gmail.com>
2022-07-20 23:30:31 -0600

    testing webhook call

---
+++ controllers/DeployController.js
@@ -0,0 +1,27 @@
+const { exec } = require("child_process");
+
+class DeployController {
+  constructor() {}
+
+  GitPull = () => {
+    const ls = spawn("git", ["pull"]);
+
+    ls.stdout.on("data", (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    ls.stderr.on("data", (data) => {
+      console.log(`stderr: ${data}`);
+    });
+
+    ls.on("error", (error) => {
+      console.log(`error: ${error.message}`);
+    });
+
+    ls.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+  };
+}
+
+module.exports = DeployController;
--- routes/deploy.js
+++ routes/deploy.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const { exec } = require("child_process");
+// const { exec } = require("child_process");
+const DeployController = require("../controllers/DeployController");
+
+const deployController = new DeployController();
 
 //TODO extract to controller?
 
 router.post("/repo-push-event", (req, res) => {
-  exec("git pull", (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-  });
+  //   exec("git pull", (error, stdout, stderr) => {
+  //     if (error) {
+  //       console.log(`error: ${error.message}`);
+  //       return;
+  //     }
+  //     if (stderr) {
+  //       console.log(`stderr: ${stderr}`);
+  //       return;
+  //     }
+  //     console.log(`stdout: ${stdout}`);
+  //   });
+  deployController.GitPull();
   res.status(200).send();
 });
 
