07c8c78f8a0aecdc869359039d449b149c9ef982
stevenshow <steven.schoebinger@gmail.com>
2022-07-20 23:35:37 -0600

    testing webhook call

--- routes/deploy.js
+++ routes/deploy.js
@@ -6,7 +6,7 @@ const deployController = new DeployController();
 
 router.post("/repo-push-event", (req, res) => {
   deployController.GitPull();
-  res.status(200).send();
+  res.status(200).send("nice");
 });
 
 module.exports = router;
