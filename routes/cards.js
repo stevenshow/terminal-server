a223d85aea8b931db326b9b745372e6fd68b403b
stevenshow <steven.schoebinger@gmail.com>
2022-07-20 19:55:50 -0600

    code formatting

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const CardController = require("../controllers/CardController").default;
+const CardController = require("../controllers/CardController");
 
 const cardController = new CardController();
 
