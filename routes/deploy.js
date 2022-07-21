64ae56eeda6d867ade4a7579388072f1e7094053
Steven Schoebinger <steven.schoebinger@gmail.com>
2022-07-21 09:21:58 -0600

    attempting to validate webhook payload

--- routes/deploy.js
+++ routes/deploy.js
@@ -1,5 +1,5 @@
 // const express = require("express");
-// const router = express.Router();
+const router = express.Router();
 // const DeployController = require("../controllers/DeployController");
 
 // const deployController = new DeployController();
