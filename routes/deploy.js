69a5553671af472668117e8eaff52baa1bfb10cf
stevenshow <steven.schoebinger@gmail.com>
2022-07-20 23:19:35 -0600

    testing webhook call

--- routes/deploy.js
+++ routes/deploy.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const { exec } = require("child_process");
 
 router.post("/repo-push-event", (req, res) => {
+  exec("ls -la", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
   res.status(200).send("good one");
 });
 
