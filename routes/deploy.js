c35ca292b5f2b1049b607c4906548326a646aa23
stevenshow <steven.schoebinger@gmail.com>
2022-07-20 23:22:02 -0600

    testing webhook call

---
+++ routes/deploy.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const { exec } = require("child_process");
+
+//TODO extract to controller?
+router.post("/repo-push-event", (req, res) => {
+  exec("git pull", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+  res.status(200).send();
+});
+
+module.exports = router;
