8a61091c8f7dac927b56e7e73b05a9502da2281a
stevenshow <steven.schoebinger@gmail.com>
2022-07-20 23:33:00 -0600

    testing webhook call

--- routes/deploy.js
+++ routes/deploy.js
@@ -5,17 +5,6 @@ const DeployController = require("../controllers/DeployController");
 const deployController = new DeployController();
 
 router.post("/repo-push-event", (req, res) => {
-  //   exec("git pull", (error, stdout, stderr) => {
-  //     if (error) {
-  //       console.log(`error: ${error.message}`);
-  //       return;
-  //     }
-  //     if (stderr) {
-  //       console.log(`stderr: ${stderr}`);
-  //       return;
-  //     }
-  //     console.log(`stdout: ${stdout}`);
-  //   });
   deployController.GitPull();
   res.status(200).send();
 });
