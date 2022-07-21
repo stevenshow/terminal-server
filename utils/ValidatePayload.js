0137fdaf401dc7e584e2411bf9405afd31f2db85
stevenshow <steven.schoebinger@gmail.com>
2022-07-20 23:09:59 -0600

    testing webhook call

--- utils/ValidatePayload.js
+++ utils/ValidatePayload.js
@@ -4,13 +4,15 @@ const sigHashAlg = "sha256";
 
 const validatePayload = (req, res, next) => {
   if (req.method == "POST") {
-    console.log("req", req);
     const sig = Buffer.from(req.get(sigHeaderName) || "", "utf8");
     const hmac = crypto.createHmac(sigHashAlg, process.env.CONFIG_SECRET);
     const digest = Buffer.from(
       sigHashAlg + "=" + hmac.update(req.rawBody).digest("hex"),
       "utf8"
     );
+    console.log("sig", sig);
+    console.log("hmac", hmac);
+    console.log("digest", digest);
 
     if (sig.length !== digest.length || !crypto.timingSafeEqual(digest, sig)) {
       return next(
