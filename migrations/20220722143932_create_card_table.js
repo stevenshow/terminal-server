c42b1bf45d67e42c05ec33d4e6dc3986e6251a55
Steven Schoebinger <steven.schoebinger@gmail.com>
2022-07-22 08:45:08 -0600

    setting up migrations

--- migrations/20220722143932_create_card_table.js
+++ migrations/20220722143932_create_card_table.js
@@ -2,14 +2,18 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("home_cards", (t) => {
+    t.increments("id").primary();
+    t.string("name", 100);
+    t.text("content");
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable("home_cards");
 };
