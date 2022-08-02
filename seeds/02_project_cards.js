9f20e4a11aad7c2e001fdc2bb043c2c53b102040
Steven Schoebinger <steven.schoebinger@gmail.com>
2022-08-02 09:06:20 -0600

    removed array due to issues with seed and db

--- seeds/02_project_cards.js
+++ seeds/02_project_cards.js
@@ -12,11 +12,8 @@ exports.seed = async function (knex) {
       language: 'Node.js',
       description:
         'I wanted to store the data for this website externally as well as learn how to host a website/server on my own hardware so that I can have more control over it. This led to creating this server with Node.js and hosting it on my Raspberry Pi. I was new to port forwarding and proxying, but I learned a lot about how our computers are accessed externally and internally. This server has migration and seed files for a MariaDB Database that is hosted on the Pi as well. This allows the server to be deployed on another device and still be able to populate the correct data.',
-      takeaway: [
-        'Create REST API',
-        'Set up Reverse Proxy with NGINX',
-        'Set up Dynamic DNS to route to Raspberry Pi',
-      ],
+      takeaway:
+        'Create REST API, Set up Reverse Proxy with NGINX, Set up Dynamic DNS to route to Raspberry Pi',
     },
     {
       id: 2,
@@ -24,7 +21,7 @@ exports.seed = async function (knex) {
       language: 'Python',
       description:
         'This project was one that I started because my wife loves anything and everything about cats. I decided to create a program that would run on my Raspberry Pi that would gather a cat photo and a cat fact from two APIs each morning, attach them both to an email and send them to my wife each morning.',
-      takeaway: ['Consume an API', 'Cron scheduling'],
+      takeaway: 'Consume an API, Cron scheduling',
     },
     {
       id: 3,
@@ -32,7 +29,7 @@ exports.seed = async function (knex) {
       language: 'MySQL',
       description:
         'I created a database to store recipes that my wife could then utilize to easily locate what she wanted. I focused on allowing it to be searchable by food type, cook time, ingredients needed, etc.',
-      takeaway: ['Properly set up database', 'Query database'],
+      takeaway: 'Properly set up database, Query database',
     },
     {
       id: 4,
@@ -40,7 +37,7 @@ exports.seed = async function (knex) {
       language: 'Python',
       description:
         "I had recently learned about data collection in a python course I was taking, and decided I would gather my own. I created a program that runs everyday, on the hour, and collects weather data from a local weather station's API. I then store that in a CSV file for consumption.",
-      takeaway: ['Data Persistence'],
+      takeaway: 'Data Persistence',
     },
   ]);
 };
