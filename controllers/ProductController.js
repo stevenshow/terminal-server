dab3ff43dad08a2e8e96ff23d35eae81ee2425db
stevenshow <steven.schoebinger@gmail.com>
2022-08-07 13:50:44 -0600

    added location

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -8,9 +8,12 @@ class ProductController {
     let prices = {};
     try {
       const { data } = await axios.get(
-        'https://www.smithsfoodanddrug.com/p/powerade-mountain-berry-blast-electrolyte-vitamin-sports-drink/0004900007935?fulfillment=PICKUP&searchType=default_search'
+        'https://www.smithsfoodanddrug.com/p/powerade-mountain-berry-blast-electrolyte-vitamin-sports-drink/0004900007935'
       );
       const $ = cheerio.load(data);
+
+      const location = $('.CurrentModality-vanityName').text();
+      prices.location = location;
       const pickup = $('label[for=PICKUP] data[typeOf=Price]');
       pickup.map((i, item) => (prices['pickup'] = item.attribs.value));
       const delivery = $('label[for=DELIVERY] data[typeOf=Price]');
