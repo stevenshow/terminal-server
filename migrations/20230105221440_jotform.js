b4536944d588bad8a0b41e96930227ec75b22d35
Steven Schoebinger <steven.schoebinger@gmail.com>
2023-01-05 15:20:55 -0700

    added migration file

--- migrations/20230105221440_jotform.js
+++ migrations/20230105221440_jotform.js
@@ -2,14 +2,22 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable('jotform', (table) => {
+    table.increments('id').primary();
+    table.string('formId', 30);
+    table.string('name', 50);
+    table.string('ip', 18);
+    table.string('description', 500);
+    table.string('type', 15);
+    table.string('website', 30);
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable('jotform');
 };
