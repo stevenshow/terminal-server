48437ca1f1610299657a029fbf74437df7f7fd95
stevenshow <steven.schoebinger@gmail.com>
2023-01-10 18:49:13 -0700

    use controller

--- routes/pico.js
+++ routes/pico.js
@@ -7,7 +7,7 @@ const picoController = new PicoController();
 
 router.post('/humidity', async (req, res) => {
   try {
-    console.log(req.body);
+    picoController.InsertReading(req.body);
     res.status(200).send('ok');
   } catch {
     console.error('failed to send humidity data');
