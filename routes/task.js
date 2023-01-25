facfb129ca2f3822bc6bff81473d0246d418c695
Steven Schoebinger <steven.schoebinger@gmail.com>
2023-01-25 16:11:38 -0700

    grab boardId from body

--- routes/task.js
+++ routes/task.js
@@ -5,8 +5,7 @@ const TaskController = require('../controllers/TaskController');
 const taskController = new TaskController();
 
 router.get('/board', async (req, res) => {
-  console.log(req);
-  const { BoardId } = req.query;
+  const { BoardId } = req.body;
   res.status(200).send(await taskController.GetBoardItems({ BoardId }));
 });
 
