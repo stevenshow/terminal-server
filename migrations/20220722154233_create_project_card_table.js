f2dd52835fd21a2f466088f0eb550f1f3765685f
Steven Schoebinger <steven.schoebinger@gmail.com>
2023-02-15 09:19:11 -0700

    fix name

--- migrations/20220722154233_create_project_card_table.js
+++ migrations/20220722154233_create_project_card_table.js
@@ -3,7 +3,7 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  return knex.schema.hasTable('home_cards').then((exists) => {
+  return knex.schema.hasTable('project_cards').then((exists) => {
     if (!exists) {
       return knex.schema.createTable('project_cards', (table) => {
         table.increments('id').primary();
